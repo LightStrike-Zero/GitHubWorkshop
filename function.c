7ee3875b37f5147ca0a02e10100c0838588f1a38
Shaunzy <68426146+LightStrike-Zero@users.noreply.github.com>
2025-03-17 22:24:30 +0800

    First commit

---
+++ function.c
@@ -0,0 +1,6 @@
+#include "function.h"
+#include <stdio.h>
+
+void myFunction(){
+    printf("I am C file, hear me roar!");
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "function.h"
+
+int main()
+{
+    std::cout << "Hello GitHub Workshop!" << std::endl;
+
+    // what
+    myFunction();
+
+    // Someone add something here..
+    // code is so lonely
+
+    return 0;
+}
